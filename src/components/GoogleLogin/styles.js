fab5f7f7db6383a324d09cdb393e8d8855d19860
vvillbii <will.bowles90@gmail.com>
2023-02-15 23:55:23 -0500

    center google login button

--- src/components/GoogleLogin/styles.js
+++ src/components/GoogleLogin/styles.js
@@ -3,14 +3,17 @@ import {colors} from '../../utils/colors';
 
 export const styles = StyleSheet.create({
   container: {
+    marginTop: 10,
     backgroundColor: colors.darkGrey,
     paddingVertical: 20,
     paddingHorizontal: 8,
     borderRadius: 14,
     width: '40%',
+    alignSelf: 'center',
   },
   googleIcon: {
     width: 30,
     height: 30,
+    alignSelf: 'center',
   },
 });
