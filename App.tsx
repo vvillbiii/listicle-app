a8dda25a5ee889c12071417d52dc5c1197fedf72
vvillbii <will.bowles90@gmail.com>
2023-02-20 20:36:23 -0500

    remove react navigation header

--- App.tsx
+++ App.tsx
@@ -31,9 +31,21 @@ function App(): JSX.Element {
   return (
     <NavigationContainer theme={MyTheme}>
       <Stack.Navigator initialRouteName="Splash">
-        <Stack.Screen name="Splash" component={Splash} />
-        <Stack.Screen name="Signin" component={Signin} />
-        <Stack.Screen name="Signup" component={Signup} />
+        <Stack.Screen
+          name="Splash"
+          component={Splash}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Signin"
+          component={Signin}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Signup"
+          component={Signup}
+          options={{headerShown: false}}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
