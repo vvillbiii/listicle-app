ba03d1d0fc674d7514c4d382840a350148a5ab6c
vvillbii <will.bowles90@gmail.com>
2023-02-22 18:31:37 -0500

    add search feature to header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Image, Pressable, Text, View} from 'react-native';
+import {Input} from '../Input';
 import {styles} from './styles';
 
 export const Header = ({
@@ -10,37 +11,46 @@ export const Header = ({
   showBack,
   showLogout,
 }) => {
-  return (
-    <View style={styles.container}>
-      {showBack ? (
-        <Pressable hitSlop={20} onPress={onBackPress}>
-          <Image
-            style={styles.icon}
-            source={require('../../assets/back.png')}
-          />
-        </Pressable>
-      ) : showSearch ? (
-        <Pressable hitSlop={20} onPress={onBackPress}>
-          <Image
-            style={styles.icon}
-            source={require('../../assets/ri_search-2-line.png')}
-          />
-        </Pressable>
-      ) : (
-        <View style={styles.space}></View>
-      )}
-      <Text style={styles.title}>{title}</Text>
+  const [showSearchInput, setShowSearchInput] = useState(false);
 
-      {showLogout ? (
-        <Pressable hitSlop={20} onPress={onBackPress}>
-          <Image
-            style={styles.icon}
-            source={require('../../assets/Logout.png')}
-          />
-        </Pressable>
-      ) : (
-        <View style={styles.space}></View>
-      )}
+  const onSearchClick = () => {
+    setShowSearchInput(s => !s);
+  };
+  return (
+    <View>
+      <View style={styles.container}>
+        {showBack ? (
+          <Pressable hitSlop={20} onPress={onBackPress}>
+            <Image
+              style={styles.icon}
+              source={require('../../assets/back.png')}
+            />
+          </Pressable>
+        ) : showSearch ? (
+          <Pressable hitSlop={20} onPress={onSearchClick}>
+            <Image
+              style={styles.icon}
+              source={require('../../assets/ri_search-2-line.png')}
+            />
+          </Pressable>
+        ) : (
+          <View style={styles.space}></View>
+        )}
+        <Text style={styles.title}>{title}</Text>
+
+        {showLogout ? (
+          <Pressable hitSlop={20} onPress={onBackPress}>
+            <Image
+              style={styles.icon}
+              source={require('../../assets/Logout.png')}
+            />
+          </Pressable>
+        ) : (
+          <View style={styles.space}></View>
+        )}
+      </View>
+
+      {showSearchInput ? <Input placeholder="Type your keyword" /> : null}
     </View>
   );
 };
